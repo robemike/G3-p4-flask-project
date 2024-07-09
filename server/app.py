5ec16b3034eef6d8f025da86efbe69202a6d0769
robemike <robemike1135@gmail.com>
2024-07-09 14:39:44 +0300

    initialize API

--- server/app.py
+++ server/app.py
@@ -1,1 +1,17 @@
-from models import db, User
+#!/usr/bin/env python
+
+from flask import Flask 
+from flask_migrate import Migrate
+
+from models import db, Member
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app=app, db=db)
+db.init_app(app)
+
+if __name__ == "__main":
+    app.run(port=5555, debug=True)
